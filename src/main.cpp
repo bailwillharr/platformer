5f4ede422ba29d2228010ba10030c73a8fe3ed6e
bailwillharr <baileyharrison04@outlook.com>
2023-02-14 13:37:26 +0000

    Get window working

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
+#include "window.hpp"
+
 #include <cstdio>
 #include <cstdlib>
 
 int main(int, char*[])
 {
 	printf("Hello world!\n");
+
+	engine::Window win("Platformer", false, true);
+
+	while (win.isRunning()) {
+
+		if (win.getKeyPress(engine::inputs::Key::K_ESCAPE)) {
+			win.setCloseFlag();
+		}
+
+		win.getInputAndEvents();
+	}
+
 	return EXIT_SUCCESS;
 }
